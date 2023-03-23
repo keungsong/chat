b31fffb1fa66c2afdea96e8d70336fefd93fd8fe
KeungXiongV <74707079+KeungXiongV@users.noreply.github.com>
2023-03-23 09:54:53 +0700

    update bot

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from './assets/bot.png';
+import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
@@ -80,7 +80,7 @@ const handleSubmit = async(e)=>{
     loader(messageDiv);
 
     // fetch data from server -> bot's response
-    const response = await fetch('https://chat-imyx.onrender.com/',{
+    const response = await fetch('https://chat-imyx.onrender.com',{
     method: 'POST',
     headers:{
         'Content-Type' : 'application/json',
